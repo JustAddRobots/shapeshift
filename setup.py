e1ab9b0c4db17d493160533c3f41c824bad7a007
Roderick Constance <justaddrobots@icloud.com>
2023-01-25 20:27:03 -0800

    ISSUE-035: Removed additional modules, they will be symlinked

--- setup.py
+++ setup.py
@@ -31,9 +31,6 @@ setup(
         "shapeshift",
         "shapeshift.blender",
         "shapeshift.substance_painter"
-        "shapeshift.substance_painter.modules"
-        "shapeshift.substance_painter.plugins"
-        "shapeshift.substance_painter.startup"
     ],
     entry_points={
         "console_scripts": [
