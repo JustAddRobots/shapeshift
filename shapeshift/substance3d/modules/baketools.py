2a81404d20268ddef7f9c39b24f7c0a57dde5cc0
Roderick Constance <justaddrobots@icloud.com>
2023-02-06 01:35:39 -0800

    ISSUE-035: Fixed mesh map baking config and exec

--- shapeshift/substance3d/modules/baketools.py
+++ shapeshift/substance3d/modules/baketools.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
 import datetime
-import os.walk
+import os
 import platform
+import pprint
+import re
 import tempfile
+from pathlib import Path
 
 # import substance_painter.exception as painter_exc
 import substance_painter.logging as painter_log
@@ -17,6 +20,11 @@ class MeshMap():
     def __init__(self, mesh_file_path):
         self._mesh_file_path = mesh_file_path
         self._sbsbaker_path = self._get_sbsbaker_path()
+        self._baked_mesh_maps = self._get_baked_mesh_maps()
+
+    @property
+    def baked_mesh_maps(self):
+        return self._baked_mesh_maps
 
     def _get_substance_designer_path(self):
         substance_designer_path = ""
@@ -27,6 +35,12 @@ class MeshMap():
                 """grep ^/Applications"""
             )
         substance_designer_path = command.get_shell_cmd(cmd)["stdout"]
+        substance_designer_path = substance_designer_path.strip()
+        painter_log.log(
+            painter_log.DBG_INFO,
+            "shapeshift",
+            f"substance_designer_path: {substance_designer_path}"
+        )
         return substance_designer_path
 
     def _get_sbsbaker_path(self):
@@ -35,8 +49,13 @@ class MeshMap():
         if substance_designer_path:
             for path, directory, files in os.walk(substance_designer_path):
                 if "sbsbaker" in files:
-                    sbsbaker_path = path
+                    sbsbaker_path = f"{path}/sbsbaker"
                     break
+        painter_log.log(
+            painter_log.DBG_INFO,
+            "shapeshift",
+            f"sbsbaker_path: {sbsbaker_path}"
+        )
         return sbsbaker_path
 
     def _get_tmp_bake_dir(self):
@@ -44,6 +63,12 @@ class MeshMap():
         tmp_bake_dir = (
             f"{tmpdir}/shapeshift-{datetime.datetime.now().isoformat()}"
         )
+        painter_log.log(
+            painter_log.DBG_INFO,
+            "shapeshift",
+            f"tmp_bake_dir: {tmp_bake_dir}"
+        )
+        os.makedirs(tmp_bake_dir)
         return tmp_bake_dir
 
     def _bake_map(self, mesh_map, tmp_bake_dir):
@@ -56,30 +81,38 @@ class MeshMap():
             "--output-size 11,11",
         ]
         if mesh_map == "normal-world-space":
-            cmd.insert(0, f"{self._sbsbaker_path} {mesh_map}")
+            cmd.insert(0, f"{re.escape(self._sbsbaker_path)} {mesh_map}")
         else:
-            cmd.insert(0, f"{self._sbsbaker_path} {mesh_map}-from-mesh")
-            cmd.extend(
+            cmd.insert(0, f"{re.escape(self._sbsbaker_path)} {mesh_map}-from-mesh")
+            cmd.extend([
                 "--antialiasing 2",
                 "--use-lowdef-as-highdef true"
-            )
+            ])
         cmd = " ".join(cmd)
-        result = command.get_shell_command(cmd)
+        painter_log.log(painter_log.DBG_INFO, "shapeshift", f"cmd: {cmd}")
+        result = command.get_shell_cmd(cmd)
         return result
 
-    def _bake_mesh_maps(self):
+    def _get_baked_mesh_maps(self):
         if os.path.exists(self._mesh_file_path) and self._sbsbaker_path:
-            tmp_bake_dir = self._get_tmp_bake_dir()
-            mesh_maps = [
+            baked_mesh_maps = {}
+            maps_to_bake = (
                 "normal",
                 "normal-world-space",
                 "ambient-occlusion",
                 "curvature",
                 "position",
-            ]
-            for mesh_map in mesh_maps:
+            )
+            tmp_bake_dir = self._get_tmp_bake_dir()
+            painter_log.log(painter_log.INFO, "shapeshift", "Baking Mesh Maps...")
+            for mesh_map in maps_to_bake:
+                painter_log.log(
+                    painter_log.DBG_INFO,
+                    "shapeshift",
+                    f"Baking Map: {mesh_map}"
+                )
                 try:
-                    self._bake_map(mesh_map, tmp_bake_dir)
+                    result = self._bake_map(mesh_map, tmp_bake_dir)
                 except (OSError, ValueError) as e:
                     painter_log.log(
                         painter_log.ERROR,
@@ -87,13 +120,34 @@ class MeshMap():
                         f"Bake Error, {mesh_map}: {e}"
                     )
                     # QT Alert
+                else:
+                    # Capture stderr from command-line bake tool.
+                    if result["stderr"]:
+                        regex = r"\[ERROR\]\[(.*)\](.*)"
+                        for line in result["stderr"].split("/n"):
+                            match = re.search(regex, line)
+                            if match:
+                                painter_log.log(
+                                    painter_log.ERROR,
+                                    match[1],  # channel
+                                    match[2],
+                                )
+                    else:
+                        mesh_map_file = (
+                            f"{tmp_bake_dir}/"
+                            f"{Path(self._mesh_file_path).stem}.{mesh_map}.tga"
+                        )
+                        baked_mesh_maps[mesh_map] = mesh_map_file
+            painter_log.log(painter_log.INFO, "shapeshift", "Bake Mesh Maps Done.")
+            painter_log.log(
+                painter_log.DBG_INFO,
+                "shapeshift",
+                pprint.saferepr(baked_mesh_maps)
+            )
         else:
             pass
             # QT alert
-        return None
-
-    def bake_mesh_maps(self):
-        self._bake_mesh_maps()
+        return baked_mesh_maps
 
 
 # texset = tex.TextureSet.from_name("M_Structure_Office_Wall_A")
